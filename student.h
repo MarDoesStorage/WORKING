669c755644d1f6e22fe771310eb4350b2c8ed0d8
convotgl <103974080+convotgl@users.noreply.github.com>
2022-06-29 01:00:54 +1200

    Update student.h

--- student.h
+++ student.h
@@ -12,6 +12,8 @@ typedef struct student {
 	std::string lastName;
 	std::string classes;
 	std::string phNumber;
+	std::string class1, class2, class3, class4, class5;
+	std::string grade1, grade2, grade3, grade4, grade5;
 };
 
 void studentRegister() {
@@ -26,7 +28,29 @@ void studentRegister() {
 	std::cout << "Enter Students Phone Number: ";
 	std::cin >> stu1.phNumber;
 
-	outfile << stu1.firstName << stu1.lastName << stu1.classes << stu1.phNumber << std::endl;
+	outfile << stu1.firstName << " " << stu1.lastName << " " << stu1.classes << " " << stu1.phNumber << std::endl;
+	std::cout << "New Student Information has been Added! \n";
+	outfile.close();
+	return;
+}
+
+void studentGrades() {
+	std::fstream outfile("grades.txt", std::ios::app);
+	student stu1;
+	std::cout << "Enter Students Classes: \n";
+	std::cin >> stu1.class1 >> stu1.class2 >> stu1.class3 >> stu1.class4 >> stu1.class5;
+	std::cout << "Enter Grade for: " << stu1.class1;
+	std::cin >> stu1.grade1;
+	std::cout << "Enter Grade for: " << stu1.class2;
+	std::cin >> stu1.grade2;
+	std::cout << "Enter Grade for: " << stu1.class3;
+	std::cin >> stu1.grade3;
+	std::cout << "Enter Grade for: " << stu1.class4;
+	std::cin >> stu1.grade4;
+	std::cout << "Enter Grade for: " << stu1.class5;
+	std::cin >> stu1.grade5;
+
+	outfile << stu1.class1 << ": " << stu1.grade1 << ", " << stu1.class2 << ": " << stu1.grade2 << ", " << stu1.class3 << ": " << stu1.grade3 << ", " << stu1.class4 << ": " << stu1.grade4 << ", " << stu1.class5 << ": " << stu1.grade5 << std::endl;
 	std::cout << "New Student Information has been Added! \n";
 	outfile.close();
 	return;
